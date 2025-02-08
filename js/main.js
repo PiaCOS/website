2e260d3b0d113f2f1fb922cd1853f8b0d99ee468
piaCOS <pia.cosneau@gmail.com>
2025-02-08 15:20:22 +0100

    feat: fmt and color changes

--- js/main.js
+++ js/main.js
@@ -1,45 +1,50 @@
-function generateFrames(elem, padding) {
-    let paddedLength = length - padding*2;
-    let repeated = elem.repeat(Math.ceil(length / elem.length) + 1)
+function generateFrames(seed, paddingLength) {
+    let paddedLength = BOX_LENGTH - 2*paddingLength;
+    let repeatCount = Math.ceil(BOX_LENGTH / seed.length) + 1;
+    let repeatedSeed = seed.repeat(repeatCount);
+
+    let padding = " ".repeat(paddingLength);
     let frames = []
 
-    for (let i = 0; i < elem.length; i++) {
-        let lineRepeat = repeated.slice(i, i+paddedLength);
-        let spaces = " ".repeat(padding);
-        frames.push(spaces + lineRepeat + spaces + "\n");
+    for (let i = 0; i < seed.length; i++) {
+        let trimed = repeatedSeed.slice(i, i + paddedLength);
+        frames.push(`${padding}${trimed}${padding}\n`);
     }
     return frames
 }
 
-const length = 29;
+const BOX_LENGTH = 29;
 
-let index = 0;
-let elements = [
+const ELEMENTS = [
     generateFrames("NWnwu:>>=-", 4),
-    generateFrames("|o+=~-", 9),
+    generateFrames("|p+=~-|i+=~-|a+=~-|c+=~-|o+=~-|s+=~-", 9),
     generateFrames(":>~-. ", 12),
-    generateFrames("@NGDx+*>=~~-.", 0),
 ]
-let texts = [
+const PATTERN = [
     0, 0, 
-    1, 1, 1, 
+    1, 1, 
     2, 2, 2, 
     1, 1, 
     0, 0, 
     2, 2,
     0, 0,
     1,
-    2, 2,
+    2,
 ];
 
-function updateText() {
-    let frame = "";
-    for (let i = 0; i < texts.length; i++) {
-        element = elements[texts[i]];
-        frame = frame + element[(index+i) % element.length]
-    }
-    document.getElementById("towerBox").innerText = frame;
-    index = index + 1;
+function createUpdater() {
+    let index = 0;
+
+    return function() {
+        let frame = "";
+        for (let i = 0; i < PATTERN.length; i++) {
+            element = ELEMENTS[PATTERN[i]];
+            frame += element[(index + i) % element.length]
+        }
+        document.getElementById("towerBox").innerText = frame;
+        index ++;
+    };
 }
 
+const updateText = createUpdater();
 setInterval(updateText, 75);
